80b3048e4692ebf983c001846263978cf0ab7168
Peter Holló <lordpejdar@gmail.com>
2019-03-03 21:04:20 +0100

    Implement collect files

---
+++ src/collectFiles.js
@@ -0,0 +1,23 @@
+const glob = require("glob");
+
+/**
+ * Collect files for later OCR processing
+ * @param {string} dir - directory to start search
+ * @param {Array<string>} extnames - file extensions to search for
+ * @returns Promise.<Array<string>|Error>
+ * TODO: add flag to ignore still processed files
+ */
+function collectFiles(dir, ...extnames) {
+  return new Promise((resolve, reject) => {
+    const extensions = extnames.length > 0 ? extnames : ["png", "jpg", "jpeg"];
+    glob(`**/*.{${[...extensions]}}`, { cwd: dir }, (err, files) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(files);
+      }
+    });
+  });
+}
+
+module.exports = collectFiles;
---
+++ tests/collectFiles.spec.js
@@ -0,0 +1,37 @@
+const { promisify } = require("util");
+const { resolve } = require("path");
+const { writeFile } = require("fs").promises;
+const mkdirp = promisify(require("mkdirp"));
+const rimraf = promisify(require("rimraf"));
+
+const collectFiles = require("../src/collectFiles");
+const sourceDir = resolve(__dirname, "testsource");
+
+beforeAll(async () => {
+  const dir1 = await mkdirp(resolve(sourceDir, randomString()));
+  const dir2 = await mkdirp(resolve(sourceDir, randomString()));
+  await writeFile(resolve(dir1, `${randomString()}.png`), "");
+  await writeFile(resolve(dir1, `${randomString()}.jpg`), "");
+  await writeFile(resolve(dir2, `${randomString()}.jpeg`), "");
+  await writeFile(resolve(dir2, `${randomString()}.txt`), "");
+});
+
+test("Collect all files of given location", async () => {
+  const results = await collectFiles(sourceDir);
+  expect(results).toHaveLength(3);
+});
+
+test("Collect all files with given extensions", async () => {
+  const results = await collectFiles(sourceDir, "jpg", "png");
+  expect(results).toHaveLength(2);
+});
+
+afterAll(async () => {
+  await rimraf(sourceDir);
+});
+
+function randomString() {
+  return Math.random()
+    .toString(36)
+    .substr(2, 5);
+}
