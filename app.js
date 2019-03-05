e66444dd800c1778a08fb2d8e3c1fb5959455539
Peter Holló <lordpejdar@gmail.com>
2019-03-05 22:53:19 +0100

    Implement upload

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 
 const { promisify } = require("util");
-const chalk = require("chalk");
 const debug = require("debug");
 const figlet = promisify(require("figlet"));
 const yargs = require("yargs");
 const { collectFiles } = require("./src");
 const { name } = require("./package.json");
+const { Storage } = require("@google-cloud/storage");
+const readline = require("readline");
+const { join } = require("path");
 
 const log = debug("main");
 
@@ -15,15 +17,50 @@ console.clear();
 async function run() {
   const title = name.toUpperCase();
   console.log(await figlet(title));
+
   const argv = yargs
     .option("input", { alias: "i", default: "./in" })
     .option("output", { alias: "o", default: "./out" })
-    .env("OCR")
+    .option("bucket", { alias: "b" })
+    .option("keyFilename", { alias: "k" })
+    .env()
     .locale("en")
     .wrap(80).argv;
-  log("argv:", { ...argv });
+
+  const { input, bucket, output, keyFilename } = argv;
+  log("argv:", "-i", input, "-o", output, "-b", bucket, "-k", keyFilename);
+
   const files = await collectFiles(argv.input);
-  log("files:", files);
+  const storage = new Storage({ keyFilename });
+  const fileUploads = files.map(file => {
+    return new Promise(resolve => {
+      storage
+        .bucket(bucket)
+        .upload(join(input, file), {
+          destination: file,
+          gzip: true,
+          metadata: { cacheControl: "public, max-age=31536000" }
+        })
+        .then(() => {
+          // @ts-ignore
+          process.stdout.clearLine();
+          // @ts-ignore
+          process.stdout.cursorTo(0);
+          process.stdout.write(
+            `${file} uploaded (${counter()}/${files.length})`
+          );
+          resolve();
+        });
+    });
+  });
+  await Promise.all(fileUploads);
 }
 
-run().then(() => console.log("Done!"));
+const counter = (() => {
+  let count = 0;
+  return (reset = false) => {
+    return (count = reset ? 0 : count + 1);
+  };
+})();
+
+run().then(() => console.log("\nDone!"));
