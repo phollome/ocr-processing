4131188d3551d8cee119ff5d25b449a213b7766f
Peter Holló <lordpejdar@gmail.com>
2019-03-09 23:29:21 +0100

    Implement processing and store results

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const { promisify } = require("util");
 const debug = require("debug");
 const figlet = promisify(require("figlet"));
 const yargs = require("yargs");
-const { collectFiles } = require("./src");
+const { collectFiles, combineResult, storeResults } = require("./src");
 const { name } = require("./package.json");
 const { Storage } = require("@google-cloud/storage");
 const readline = require("readline");
 const { join } = require("path");
+const { ImageAnnotatorClient } = require("@google-cloud/vision");
 
 const log = debug("main");
 
@@ -30,6 +31,7 @@ async function run() {
   const { input, bucket, output, keyFilename } = argv;
   log("argv:", "-i", input, "-o", output, "-b", bucket, "-k", keyFilename);
 
+  console.log("\nUpload images");
   const files = await collectFiles(argv.input);
   const storage = new Storage({ keyFilename });
   const fileUploads = files.map(file => {
@@ -54,6 +56,29 @@ async function run() {
     });
   });
   await Promise.all(fileUploads);
+
+  console.log("\nProcess images");
+  counter(true);
+  const client = new ImageAnnotatorClient();
+  const detections = files.map(file => {
+    return new Promise(resolve => {
+      client.documentTextDetection(`gs://${bucket}/${file}`).then(result => {
+        const combinedResult = combineResult(result);
+        // @ts-ignore
+        process.stdout.clearLine();
+        // @ts-ignore
+        process.stdout.cursorTo(0);
+        process.stdout.write(
+          `${file} processed (${counter()}/${files.length})`
+        );
+        resolve({ source: file, data: combinedResult });
+      });
+    });
+  });
+  const results = await Promise.all(detections);
+
+  console.log("\nStore results");
+  await storeResults(output, results);
 }
 
 const counter = (() => {
